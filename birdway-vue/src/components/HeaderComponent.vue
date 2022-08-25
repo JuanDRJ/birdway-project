c5321ee8bc05c8358332655c3b5dfe888ee2c7ff
Juan David Ramirez Ortiz <iamjuandavid21@gmail.com>
2022-08-25 18:32:05 -0500

    creación de drop down

--- birdway-vue/src/components/HeaderComponent.vue
+++ birdway-vue/src/components/HeaderComponent.vue
@@ -24,6 +24,15 @@
       </button>
     </div>
     <!--  DROP DOWN -->
+    <div class="dropdown">
+      <button class="dropbtn">&#x25BC;</button>
+      <div class="dropdown-content">
+      <a href="#">Link 1</a>
+      <a href="#">Link 2</a>
+      <a href="#">Link 3</a>
+    </div>
+</div>
+
   </nav>
 </template>
 
@@ -127,6 +136,45 @@ input:focus {
     align-items: center  ;
 } */
 /* DROP DOWN STYLE */
+.dropbtn {
+  background: white;
+  color: #4CAF50;
+  padding: 16px;
+  font-size: 16px;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #f9f9f9;
+  min-width: 160px;
+  box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: black;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+}
+
+.dropdown-content a:hover {background-color: #f1f1f1}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: #ffffff;
+}
 </style>
 
 
