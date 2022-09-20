98b4a3b994fc1f35a15ca813abab0748ec75d44e
JuanDRJ <iamjuandavid21@gmail.com>
2022-09-20 15:46:54 -0500

    Instalación e implementación de consumo API

--- src/components/RutasComponent.vue
+++ src/components/RutasComponent.vue
@@ -1,7 +1,8 @@
 <template>
         <div>
-                <MainButton inner-text="Rutas buton"/>
-                <SecondaryButton inner-text="Secondary rutas"/>
+                <!-- <MainButton inner-text="Rutas buton"/>
+                <SecondaryButton inner-text="Secondary rutas"/> -->
+                <BirdListLocation></BirdListLocation>
         </div>
 </template>
 
@@ -14,4 +15,5 @@
 <script setup>
         import MainButton from './atoms/buttons/MainButton.vue';
 import SecondaryButton from './atoms/buttons/SecondaryButton.vue';
+import BirdListLocation from './organism/BirdListLocation.vue';
 </script>
---
+++ src/components/organism/BirdListLocation.vue
@@ -0,0 +1,47 @@
+<template>
+    <div>
+        <table>
+            <tbody>
+                <tr>
+                    <td>tabla</td>
+                    <td>tabla</td>
+                    <td>tabla</td>
+                    <td>tabla</td>
+                </tr>
+            </tbody>
+        </table>
+
+    </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+    import axios from 'axios'
+
+    export default{
+        data(){
+            return{
+                todos:null
+            }
+        },
+        mounted(){
+            console.log('hola vue desde mounted')
+            this.getTodos();
+        },
+        methods:{
+            getTodos(){
+                console.log('Hola vue desde methods')
+                axios
+                 .get('https://catfact.ninja/fact')
+                 .then(response =>{
+                    console.log(response)
+                 })
+                 .catch (e=> console.log(e))
+            }
+        }
+    
+    }
+</script>
