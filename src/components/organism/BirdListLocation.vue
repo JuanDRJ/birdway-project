8766ee03543524d61211b8f46fa45646841eab2a
JuanDRJ <iamjuandavid21@gmail.com>
2022-09-22 10:48:01 -0500

    Rendering de lista API

--- src/components/organism/BirdListLocation.vue
+++ src/components/organism/BirdListLocation.vue
@@ -3,8 +3,10 @@
     <table>
       <tbody>
         <tr v-for="todo in todos" :key="todo.id">
-          <td>{{ todo.id }}</td>
-          <td>{{ todo.title }}</td>
+          <td><a href="#">{{ todo.comName }}</a></td>
+          <td>{{ todo.sciName }}</td>
+          <td>{{ todo.locName}}</td>x 
+          <td>{{ todo.obsDt}}</td>
         </tr>
       </tbody>
     </table>
@@ -20,7 +22,7 @@
        data(){
            return{
                todos:null,
-               /* regionCode: 'CO-CAL' */
+               regionCode: 'CO-CAL'
            }
        },
        mounted(){
@@ -29,10 +31,10 @@
        },
        methods:{
            getTodos(){
-               /* let code = this.regionCode; */
+               let API= 'https://api.ebird.org/v2/data/obs/CO-CAL/recent'
                console.log('Hola vue desde methods')
                axios
-                .get(`https://api.ebird.org/v2/data/obs/CO-CAL/recent`,{
+                .get(API,{
                 headers: {  'X-eBirdApiToken' : '33ig95hlu56o'
                 }
             
