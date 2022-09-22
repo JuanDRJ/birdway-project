ee0f5e0956c03bd29007c02cb8d259e213a4e3fb
JuanDRJ <iamjuandavid21@gmail.com>
2022-09-21 21:46:27 -0500

    Conexión API

--- src/components/organism/BirdListLocation.vue
+++ src/components/organism/BirdListLocation.vue
@@ -1,46 +1,49 @@
 <template>
-    <div>
-        <table>
-            <tbody>
-                <tr v-for="todo in todos" :key="todo.id">
-                    <td>{{todo.id}}</td>
-                    <td>{{todo.title}}</td>
-                </tr>
-            </tbody>
-        </table>
-
-    </div>
+  <div>
+    <table>
+      <tbody>
+        <tr v-for="todo in todos" :key="todo.id">
+          <td>{{ todo.id }}</td>
+          <td>{{ todo.title }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
 
 <script>
-    import axios from 'axios'
+   import axios from 'axios'
 
-    export default{
-        data(){
-            return{
-                todos:null
-            }
-        },
-        mounted(){
-            console.log('hola vue desde mounted')
-            this.getTodos();
-        },
-        methods:{
-            getTodos(){
-                console.log('Hola vue desde methods')
-                axios
-                 .get('https://jsonplaceholder.typicode.com/posts')
-                 .then(response =>{
-                    console.log(response)
-                    this.todos = response.data
-                 })
-                 .catch (e=> console.log(e))
-            }
-        }
-    
-    }
+   export default{
+       data(){
+           return{
+               todos:null,
+               regionCode: 'CO-CAL'
+           }
+       },
+       mounted(){
+           console.log('hola vue desde mounted')
+           this.getTodos();
+       },
+       methods:{
+           getTodos(){
+               let code = this.regionCode;
+               console.log('Hola vue desde methods')
+               axios
+                .get(`https://api.ebird.org/v2/data/obs/${{code}}/recent`,{
+                headers: {  'X-eBirdApiToken' : '33ig95hlu56o'
+                }
+            
+                })
+                .then(response =>{
+                   console.log(response)
+                   this.todos = response.data
+                })
+                .catch (e=> console.log(e))
+           }
+       }
+
+   }
 </script>
